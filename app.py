d381c715ad1404c94643cb2b57fd91a6289b7632
Gowtham Bharath N <barathinfo28@gmail.com>
2025-12-21 21:24:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
 import whisper
+import torch
 
 app = FastAPI()
 
-model = whisper.load_model("base")
+# FORCE CPU
+device = "cpu"
+model = whisper.load_model("tiny", device=device)
 
 @app.post("/stt")
 async def stt(file: UploadFile = File(...)):
-    with open("audio.wav", "wb") as f:
+    audio_path = "audio.wav"
+
+    with open(audio_path, "wb") as f:
         f.write(await file.read())
 
-    result = model.transcribe("audio.wav")
-    return {"text": result["text"]}
+    result = model.transcribe(audio_path)
+    return {"text": result["text"].strip()}
